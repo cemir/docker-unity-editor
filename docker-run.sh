7107646e21fa6c075be160d68095aa58743547c9
cemir <cdeloeul@gmail.com>
2017-02-27 14:13:42 +0100

    first commit

---
+++ docker-run.sh
@@ -0,0 +1,8 @@
+docker run -ti --rm \
+	-e DISPLAY=$DISPLAY \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-v /home/cem/ownCloud/homes/gamedev:/gamdev \
+	unity_new
+
+
+# -v /home/cem/ownCloud/homes/gamedev:/home/cem \
